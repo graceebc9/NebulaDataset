82b4cecdbc7e7992fc0ed10a2a25bae197263b12
G. Beaney-Colverd <gb66@cam.ac.uk>
2025-06-13 11:38:19 +0100

    update trak

--- process_region_gea.py
+++ process_region_gea.py
@@ -22,27 +22,18 @@ from src.postcode_utils import check_duplicate_primary_key, find_data_pc_joint
 import os
 import pandas as pd
 from tqdm import tqdm
-<<<<<<< HEAD
 import pickle
-
 def get_ratio(proc, epc):
-=======
-
-def get_ratio(proc, epc):
-   
-
->>>>>>> 303c8008f51ba287a5dabe57709ce26d659e290c
     dfm = proc[proc['uprn'].notna()]
     df = dfm.merge(epc, right_on='UPRN', left_on='uprn', how='inner')
 
     df['epc_form']= df['BUILT_FORM'].str.lower().str.replace('-', ' ')
 
-    match_houses= df[df.apply(lambda row: str(row['epc_form']) in str(row['premise_type']), axis=1) & (df['PROPERTY_TYPE']!='Flat')]
+    # match_houses= df[df.apply(lambda row: str(row['epc_form']) in str(row['premise_type']), axis=1) & (df['PROPERTY_TYPE']!='Flat')]
 
-<<<<<<< HEAD
-    match_houses['gross_epc'] = match_houses['gross_area'] / match_houses['TOTAL_FLOOR_AREA'] * 100 
-    match_houses['meta_res'] = match_houses['total_fl_area_meta'] / match_houses['TOTAL_FLOOR_AREA'] * 100
-    return match_houses[['gross_epc', 'premise_age_bucketed','TOTAL_FLOOR_AREA', 'meta_res', 'uprn','gross_area' , 'total_fl_area_meta', 'total_fl_area_meta_source',  'BUILT_FORM', 'premise_type', 'PROPERTY_TYPE']]
+    df['gross_epc'] = df['gross_area'] / df['TOTAL_FLOOR_AREA'] * 100 
+    df['meta_res'] = df['total_fl_area_meta'] / df['TOTAL_FLOOR_AREA'] * 100
+    return df[['gross_epc', 'premise_age_bucketed','TOTAL_FLOOR_AREA', 'meta_res', 'uprn','gross_area' , 'total_fl_area_meta', 'total_fl_area_meta_source', 'epc_form', 'BUILT_FORM', 'TENURE', 'CONSTRUCTION_AGE_BAND', 'premise_age','premise_type', 'PROPERTY_TYPE']]
 
 def save_checkpoint(data, checkpoint_path, processed_count, total_count):
     """Save checkpoint with current progress"""
@@ -82,33 +73,11 @@ def process_region(region_code, location_input_data_folder, BUILDING_PATH, PC_SH
     print('Loading epc')
     for ld in lcds:
         f = os.path.join(epc_base_path, '*/', f'*{ld}*', 'certificates*' )
-=======
-
-    match_houses['gross_epc'] = match_houses['gross_area'] / match_houses['TOTAL_FLOOR_AREA'] * 100 
-    match_houses['meta_res'] = match_houses['total_fl_area_meta'] / match_houses['TOTAL_FLOOR_AREA'] * 100
-    return match_houses[['gross_epc', 'TOTAL_FLOOR_AREA', 'meta_res', 'uprn','gross_area' , 'total_fl_area_meta', 'total_fl_area_meta_source',  'BUILT_FORM', 'premise_type', 'PROPERTY_TYPE']]
-
-
-
-def process_region(region_code, location_input_data_folder, BUILDING_PATH, PC_SHP_PATH):
-    print('Starting region', region_code)
-    print('loading ONSUD')
-    onsud_path_base = os.path.join(location_input_data_folder, 'ONS_UPRN_database/ONSUD_DEC_2022/Data')
-    onsud_path = os.path.join(onsud_path_base, f'ONSUD_DEC_2022_{region_code}.csv')    
-    onsud_data = load_onsud_data(onsud_path, PC_SHP_PATH)
-    pc_list = onsud_data[0].PCDS.unique().tolist()() 
-    lcds = onsud_data[0].LAD21CD.unique().tolist() 
-    epcl = [] 
-    print('loading epc data')
-    for ld in lcds:
-        f = os.path.join( '/Volumes/T9/2024_Data_downloads/2025_epc_database/*/', f'*{ld}*', 'certificates*' )
->>>>>>> 303c8008f51ba287a5dabe57709ce26d659e290c
         file = glob.glob( f)
         ep = pd.read_csv(file[0])
         epcl.append(ep)
     
     epc = pd.concat(epcl, ignore_index=True)
-<<<<<<< HEAD
     epc['INSPECTION_DATE'] = pd.to_datetime(epc['INSPECTION_DATE'])
 
     # Keep only the most recent record for each UPRN
@@ -123,19 +92,11 @@ def process_region(region_code, location_input_data_folder, BUILDING_PATH, PC_SH
     
     with tqdm(total=total_postcodes, initial=start_index, desc="Processing PCs") as pbar:
         for i, pc in enumerate(pc_list_to_process, start=start_index):
-=======
-    print(f"Loaded {len(epc)} EPC records for region {region_code}")
-    res = [] 
-    error_list=[] 
-    with tqdm(total=len(pc_list), desc="Processing PCs") as pbar:
-        for pc in pc_list:
->>>>>>> 303c8008f51ba287a5dabe57709ce26d659e290c
             try:
                 uprn_match = find_data_pc_joint(pc, onsud_data, input_gpk=BUILDING_PATH)
                 proc = pre_process_building_data(uprn_match) 
                 match_df = get_ratio(proc, epc) 
                 match_df['postcode'] = pc
-<<<<<<< HEAD
                 res.append(match_df)
                 
                 # Save checkpoint every checkpoint_interval postcodes
@@ -156,18 +117,10 @@ def process_region(region_code, location_input_data_folder, BUILDING_PATH, PC_SH
         os.remove(checkpoint_path)
         print("Processing completed. Checkpoint file removed.")
     
-=======
-
-                res.append(match_df)
-            except Exception as e:
-                tqdm.write(f"Error processing {pc}: {str(e)}")
-                error_list.append((pc, str(e)))
->>>>>>> 303c8008f51ba287a5dabe57709ce26d659e290c
     return pd.concat(res, ignore_index=True)
 
     
 if __name__ == "__main__":
-<<<<<<< HEAD
     
     region_code = os.getenv('REGION')
     
@@ -195,14 +148,3 @@ if __name__ == "__main__":
     result_df = process_region(region_code, location_input_data_folder, BUILDING_PATH, PC_SHP_PATH, epc_base_path)
     output_path = os.path.join(op_path, f'processed_region_{region_code}.csv')
     result_df.to_csv(output_path, index=False)
-=======
-    region_code ='SW'
-    op_path = '/Volumes/T9/01_2025_EPC_POSTCODES/gea_comparisons/esults'
-    BUILDING_PATH = '/Volumes/T9/2024_Data_downloads/Versik_building_data/2024_03_22_updated_data/UKBuildings_Edition_15_new_format_upn.gpkg'
-
-    PC_SHP_PATH = '/Volumes/T9/2024_Data_downloads/codepoint_polygons_edina/Download_all_postcodes_2378998/codepoint-poly_5267291' 
-    location_input_data_folder = '/Volumes/T9/2024_Data_downloads/2024_11_nebula_paper_data/'
-
-    result_df = process_region(region_code, location_input_data_folder, BUILDING_PATH, PC_SHP_PATH)
-    output_path = os.path.join(op_path, f'processed_region_{region_code}.csv')
->>>>>>> 303c8008f51ba287a5dabe57709ce26d659e290c
